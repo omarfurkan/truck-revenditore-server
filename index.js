246c9d45984f69332b20a2f238ea0cd3d86bf17a
omarfurkan <omarfurkan92@gmail.com>
2022-05-13 02:32:49 +0600

    make some correction on get id api

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function run() {
 
         app.get(`/product/:id`, async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId };
+            const query = { _id: ObjectId(id) };
             const product = await truckCollection.findOne(query);
             res.send(product);
         })
