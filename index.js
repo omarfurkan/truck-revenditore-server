78ba69bfd363043ff2f1d498a41b575d0bedb08e
omarfurkan <omarfurkan92@gmail.com>
2022-05-12 12:23:10 +0600

    all set to get data

--- index.js
+++ index.js
@@ -13,12 +13,25 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.18txb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    console.log('Mongodb is set for connection')
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const truckCollection = client.db('truckRevonditore').collection('product');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = truckCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 
